144230010615adb4d925a1ee26b8b1f63c025839
Baik <bcy0729@naver.com>
2019-03-08 13:30:50 +0900

    add gulp-html-tag-include

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,33 @@
-// yarn add gulp gulp-sass gulp-postcss autoprefixer cssnano gulp-sourcemaps browser-sync
+// yarn add gulp gulp-sass gulp-postcss autoprefixer cssnano gulp-sourcemaps gulp-html-tag-include browser-sync
 
-var gulp = require("gulp"),
+const gulp = require("gulp"),
     sass = require("gulp-sass"),
     postcss = require("gulp-postcss"),
     autoprefixer = require("autoprefixer"),
     cssnano = require("cssnano"),
     sourcemaps = require("gulp-sourcemaps"),
+    fileinclude = require("gulp-html-tag-include"),
     browserSync = require("browser-sync").create();
 
-var paths = {
+const paths = {
     styles: {
         // By using styles/**/*.sass we're telling gulp to check all folders for any sass file
-        src: "src/scss/*.scss",
+        src: "./src/scss/**/*.scss",
         // Compiled files will end up in whichever folder it's found in (partials are not compiled)
-        dest: "src/css"
+        dest: "./dist/css"
+    },
+    html: {
+        // By using styles/**/*.sass we're telling gulp to check all folders for any sass file
+        src: "./src/**/*.html",
+        // Compiled files will end up in whichever folder it's found in (partials are not compiled)
+        dest: "./dist/",
     }
 
 
 
 
     // Easily add additional paths
-    // ,html: {
+    // ,include: {
     //  src: '...',
     //  dest: '...'
     // }
@@ -42,7 +49,14 @@ function style() {
         .pipe(browserSync.stream());
 }
 
+function html() {
+    return gulp
+        .src(paths.html.src)
+        .pipe(fileinclude())
+        .pipe(gulp.dest(paths.html.dest))
+        .pipe(browserSync.stream());
 
+}
 
 
 // A simple task to reload the page
@@ -55,18 +69,18 @@ function watch() {
     browserSync.init({
         // You can tell browserSync to use this directory and serve it as a mini-server
         server: {
-            baseDir: "./src"
+            baseDir: "./dist"
         }
         // If you are already serving your website locally using something like apache
         // You can use the proxy setting to proxy that instead
         // proxy: "yourlocal.dev"
     });
-    gulp.watch(paths.styles.src, style);
+    gulp.watch(paths.styles.src, style, reload);
     // We should tell gulp which files to watch to trigger the reload
-    // This can be html or whatever you're using to develop your website
+    // This can be include or whatever you're using to develop your website
     // Note -- you can obviously add the path to the Paths object
-    //gulp.watch("src/*.html", reload);
-    gulp.watch("src/*.html").on('change', browserSync.reload);
+    //gulp.watch("src/*.include", reload);
+    gulp.watch(paths.html.src, html, reload);
 }
 
 // We don't have to expose the reload function
@@ -74,17 +88,19 @@ function watch() {
 
 
 // Don't forget to expose the task!
-exports.watch = watch
-
+exports.watch = watch;
 // Expose the task by exporting it
 // This allows you to run it from the commandline using
 // $ gulp style
 exports.style = style;
+exports.html = html;
+
+
 
 /*
  * Specify if tasks run in series or parallel using `gulp.series` and `gulp.parallel`
  */
-var build = gulp.parallel(style, watch);
+const build = gulp.parallel(style, watch, html);
 
 /*
  * You can still use `gulp.task` to expose tasks
